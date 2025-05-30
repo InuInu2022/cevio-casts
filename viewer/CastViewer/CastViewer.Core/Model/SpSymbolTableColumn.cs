5ab9dd9baa7891d3565939efde93652f8bd755b7
InuInu2022 <97830071+InuInu2022@users.noreply.github.com>
2025-05-31 07:03:23 +0900

    [CastViewer]feat: support new special symbol

--- viewer/CastViewer/CastViewer.Core/Model/SpSymbolTableColumn.cs
+++ viewer/CastViewer/CastViewer.Core/Model/SpSymbolTableColumn.cs
@@ -2,34 +2,41 @@ using CevioCasts;
 
 namespace CastViewer.Core.Model;
 
-public record SpSymbolTableColumn
-(
+public record SpSymbolTableColumn(
 	Category Cat,
 	string CastName,
 	Product Product,
 	IEnumerable<DisplaySpSymbol> SpSymbols
 )
 {
-	public string Category { get; } = Cat switch
-	{
-		CevioCasts.Category.SingerSong => "Song",
-		CevioCasts.Category.TextVocal => "Talk",
-		_ => "ERROR"
-	};
-	public string? LabelFalsetto { get; } = GetName(SpSymbols, ["$", "＄", "※"]);
-	public string? LabelAtmark { get; } = GetName(SpSymbols, ["@", "＠"]);
-	public string? LabelCaret { get; } = GetName(SpSymbols, ["^", "＾"]);
-	public string? LabelPercent { get; } = GetName(SpSymbols, ["%", "％"]);
-	public string? LabelEqual { get; } = GetName(SpSymbols, ["=", "＝"]);
-	public string? LabelUnderscore { get; } = GetName(SpSymbols, ["_","＿"]);
+	public string Category =>
+		Cat switch
+		{
+			CevioCasts.Category.SingerSong => "Song",
+			CevioCasts.Category.TextVocal => "Talk",
+			_ => "ERROR",
+		};
+	public string? LabelFalsetto => GetName(SpSymbols, ["$", "＄", "※"]);
+	public string? LabelAtmark => GetName(SpSymbols, ["@", "＠"]);
+	public string? LabelCaret => GetName(SpSymbols, ["^", "＾"]);
+	public string? LabelPercent => GetName(SpSymbols, ["%", "％"]);
+	public string? LabelEqual => GetName(SpSymbols, ["=", "＝"]);
+	public string? LabelUnderscore => GetName(SpSymbols, ["_", "＿"]);
+	public string? LabelPlus => GetName(SpSymbols, ["+", "＋"]);
 
-	private static string GetName(IEnumerable<DisplaySpSymbol> spSymbols, IReadOnlyList<string> labels)
+	private static string GetName(
+		IEnumerable<DisplaySpSymbol> spSymbols,
+		IReadOnlyList<string> labels
+	)
 	{
 		var symbol = spSymbols
-			.FirstOrDefault(s => s.Symbols?.Any(v => labels.Contains(v)) == true);
-		var label = symbol?.Names?[0]
-			?? string.Empty;
-		var version = symbol?.Versions?.Count == 1 ? symbol?.Versions?.LastOrDefault() ?? string.Empty : string.Empty;
+			.FirstOrDefault(s => s.Symbols?
+				.Any(v => labels.Contains(v)) == true);
+		var label = symbol?.Names?[0] ?? string.Empty;
+		var version =
+			symbol?.Versions?.Count == 1
+				? symbol?.Versions?.LastOrDefault() ?? string.Empty
+				: string.Empty;
 		return (version is []) ? label : $"{label} ({version})";
 	}
 }
